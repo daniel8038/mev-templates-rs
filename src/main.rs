b7cd79859ca557a253bc8e83494de2e1bc0f5310
daniel <pangpangcoder@gmail.com>
2024-12-02 11:20:49 +0800

    区块信息流

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ use std::sync::Arc;
 use anyhow::Result;
 use dotenv::dotenv;
 use ethers_providers::{Provider, Ws};
-use rust::{constants::Env, streams::Event, utils::setup_logger};
-use tokio::sync::broadcast::{self, Sender};
+use rust::{
+    constants::Env,
+    streams::{stream_new_block, Event},
+    utils::setup_logger,
+};
+use tokio::{
+    sync::broadcast::{self, Sender},
+    task::JoinSet,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -15,5 +22,10 @@ async fn main() -> Result<()> {
     // ws所有权被Arc获取 或者move
     let ws_provider = Arc::new(ws);
     let (event_sender, _): (Sender<Event>, _) = broadcast::channel(512);
+    let mut set = JoinSet::new();
+    // 获取区块信息
+    set.spawn(stream_new_block(ws_provider, event_sender));
+    // 获取pending交易
+    //
     Ok(())
 }
--- src/streams.rs
+++ src/streams.rs
@@ -1,5 +1,10 @@
-use ethers::types::{Log, Transaction, U256, U64};
+use std::sync::Arc;
 
+use crate::utils::calculate_next_block_base_fee;
+use ethers::types::{Log, Transaction, U256, U64};
+use ethers_providers::{Middleware, Provider, Ws};
+use tokio::sync::broadcast::Sender;
+use tokio_stream::StreamExt;
 #[derive(Default, Debug, Clone)]
 pub struct NewBlock {
     pub block_number: U64,
@@ -11,4 +16,26 @@ pub enum Event {
     Block(NewBlock),
     PendingTx(Transaction),
     Log(Log),
+}
+// 处理区块信息流
+pub async fn stream_new_block(provider: Arc<Provider<Ws>>, event_sender: Sender<Event>) {
+    let stream = provider.subscribe_blocks().await.unwrap();
+    let mut stream = stream.filter_map(|block| match block.number {
+        Some(number) => Some(NewBlock {
+            block_number: number,
+            base_fee: block.base_fee_per_gas.unwrap_or_default(),
+            next_base_fee: U256::from(calculate_next_block_base_fee(
+                block.gas_used,
+                block.gas_limit,
+                block.base_fee_per_gas.unwrap_or_default(),
+            )),
+        }),
+        None => None,
+    });
+    while let Some(block) = stream.next().await {
+        match event_sender.send(Event::Block(block)) {
+            Ok(_) => {}
+            Err(_) => {}
+        }
+    }
 }
